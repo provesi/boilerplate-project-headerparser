53ee37cc76d1a07cabed5d2650bc247026f98eb7
Mateus Provesi <provesi27@gmail.com>
2022-06-22 17:10:35 +0000

    Update on app route "/api/whoami"

--- index.js
+++ index.js
@@ -21,8 +21,11 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  let ipAddress = req.connection.remoteAddress;
+  let language = req.headers["accept-language"];
+  let userAgent = req.headers["user-agent"];
+  res.json({ipaddress: ipAddress, language: language, software: userAgent});
 });
 
 
